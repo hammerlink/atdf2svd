1cbbbe172131c25f3770f4fbd3323b09dbd9450f
hendrik <hendrik.hamerlinck@hammernet.be>
2025-05-01 23:26:56 +0200

    run format

--- src/chip.rs
+++ src/chip.rs
@@ -55,7 +55,7 @@ pub struct RegisterGroup {
     /// Offset relative to the peripheral base address
     pub offset: usize,
     /// Indicates if this register group is a union.
-    /// 
+    ///
     /// Currently limits nested register group functionality.
     /// If removed, full nested register group support would be enabled (#4).
     pub is_union: bool,
